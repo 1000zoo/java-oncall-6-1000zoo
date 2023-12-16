8e807a589df7018b13180205b291bf0c7f45f9c7
1000zoo <cjswldn99@gmail.com>
2023-12-16 17:54:15 +0900

    feat(WorkerList): 백업 Queue를 두어서 Queue가 다 떨어졌을 때 충전할 수 있도록 수정

--- src/main/java/oncall/constants/ErrorMessage.java
+++ src/main/java/oncall/constants/ErrorMessage.java
@@ -2,6 +2,7 @@ package oncall.constants;
 
 public enum ErrorMessage {
     INVALID_WORKER_NAME("근무자의 이름은 5글자 이하여야 합니다."),
+    INVALID_TOTAL_SIZE("총 인원이 너무 적습니다."),
     INVALID_DAY_OF_WEEK("잘못된 요일입니다."),
     INVALID_MONTH("잘못된 월 입력입니다."),
     INVALID_WORKERS_INPUT("근무 인력은 5 ~ 35 명 사이여야 합니다."),
--- src/main/java/oncall/domain/WorkerList.java
+++ src/main/java/oncall/domain/WorkerList.java
@@ -1,16 +1,30 @@
 package oncall.domain;
 
 import java.util.LinkedList;
+import oncall.constants.ErrorMessage;
 import oncall.domain.date.Date;
 
 public class WorkerList {
+
+    private final LinkedList<Worker> weekdayBackup;
+    private final LinkedList<Worker> holidayBackup;
 
     private final LinkedList<Worker> weekday;
     private final LinkedList<Worker> holiday;
 
     public WorkerList(EmergencyWorkers weekday, EmergencyWorkers holiday) {
+        validate(weekday.size() + holiday.size());
         this.weekday = weekday.getWorkerQueue();
         this.holiday = holiday.getWorkerQueue();
+
+        weekdayBackup = new LinkedList<>(this.weekday);
+        holidayBackup = new LinkedList<>(this.holiday);
+    }
+
+    private void validate(int totalSize) {
+        if (totalSize < 5) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TOTAL_SIZE.getMessage());
+        }
     }
 
     public Worker pickWorker(Date date, Worker previousWorker) {
@@ -32,5 +46,17 @@ public class WorkerList {
             return holiday.poll();
         }
         return holiday.remove(1);
+    }
+
+    private void fillWeekdayWorker() {
+        if (weekday.size() < 5) {
+            weekday.addAll(weekdayBackup);
+        }
+    }
+
+    private void fillHolidayWorker() {
+        if (holiday.size() < 5) {
+            holiday.addAll(holidayBackup);
+        }
     }
 }
